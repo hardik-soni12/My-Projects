a28e9f58ea12dba4a498e7caa470e1b4c0693480
Hardik-Soni12 <imailhr1k@gmail.com>
2025-06-06 10:35:00 +0530

    added instances, main.py, requirements.txt and templates

---
+++ To-Do_app/main.py
@@ -0,0 +1,38 @@
+from asyncio import taskgroups
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Database config (SQLite)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app) 
+
+# Task model(database table)
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+# create the database tables
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task_content = request.form.get('task')
+    if task_content:
+        new_task = Task(content=task_content)
+        db.session.add(new_task)
+        db.session.commit()
+    return redirect(url_for('home'))
+
+
+if __name__ ==("__main__"):
+    app.run(debug=True)
