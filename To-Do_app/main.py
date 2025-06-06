f5828a67b42354d69aa1549275dd4bc9f4320178
Hardik-Soni12 <imailhr1k@gmail.com>
2025-06-06 10:56:46 +0530

    added delete route and del button

--- To-Do_app/main.py
+++ To-Do_app/main.py
@@ -33,6 +33,13 @@ def add():
         db.session.commit()
     return redirect(url_for('home'))
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Task.query.get_or_404(id)
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
 
 if __name__ ==("__main__"):
     app.run(debug=True)
